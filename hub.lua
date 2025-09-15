b241aabb8a10d088f8edc0c3e10aae94cd516771
Talos <Talos>
2025-09-15 18:06:39 -0300

    update hub.lua

--- hub.lua
+++ hub.lua
@@ -4,11 +4,11 @@ local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao1
 local Window = redzlib:MakeWindow({
   Title = "Talos Hub : Universal",
   SubTitle = "by thalles456u",
-  SaveFolder = "TalosHub | RedzLib.lua"
+  SaveFolder = "Ghost Hub | RedzLib.lua"
 })
 
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://1497307765", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -16,7 +16,7 @@ Window:AddMinimizeButton({
 local TabMenu = Window:MakeTab({"Menu", "cherry"})
 
 TabMenu:AddDiscordInvite({
-    Name = "Talos Hub | Community",
+    Name = "Ghost Hub | Community",
     Description = "Entrar no servidor",
     Logo = "rbxassetid://18751483361",
     Invite = "Link discord invite",
